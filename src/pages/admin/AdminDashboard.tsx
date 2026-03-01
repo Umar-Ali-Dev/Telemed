a9a6df595555cb5a52543140cc627a726bf06936
Umar-Ali <umarcodev@gmail.com>
2026-03-01 13:16:44 +0500

    provider modules of admin

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import DataTable from "react-data-table-component";
-import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import {
   HiOutlineBriefcase,
